70fca0311c2067993e41bfcb66f64e03153fc50c
oaugusto <oaugusto@outlook.com>
2019-12-09 18:19:41 -0300

    new plots added

--- FlatNet/plot_scripts/projecToR.R
+++ FlatNet/plot_scripts/projecToR.R
@@ -1,5 +1,4 @@
-#setwd("C:/Users/oaugusto/Desktop/Plots/FlatNet")
-setwd("/home/oaugusto/CBNet/PlotsScripts/csv_data/flatnet")
+setwd("/home/oaugusto/CBNet/PlotsScripts")
 
 ################################## Libraries ###################################
 
@@ -14,20 +13,17 @@ theme_set(theme_bw())
 
 ############################# Reading tables  ##################################
 
-total_work.table <- read.csv("./projector/tor/total_work.csv")
-makespan.table <-read.csv("./projector/tor/total_time.csv")
-clusters.table <-read.csv("./projector/tor/cluster.csv")
-throughput.table <-read.csv("./projector/tor/throughput.csv")
+dataset <- "random"
 
-#total_work.table <- read.csv("./projector/newTor/total_work.csv")
-#makespan.table <-read.csv("./projector/newTor/total_time.csv")
-#clusters.table <-read.csv("./projector/newTor/cluster.csv")
-#throughput.table <-read.csv("./projector/newTor/throughput.csv")
+f1 <- paste("./csv_data/flatnet/projector/", dataset, "/total_work.csv", sep = "")
+f2 <- paste("./csv_data/flatnet/projector/", dataset, "/total_time.csv", sep = "")
+f3 <- paste("./csv_data/flatnet/projector/", dataset, "/cluster.csv", sep = "")
+f4 <- paste("./csv_data/flatnet/projector/", dataset, "/throughput.csv", sep = "")
 
-#total_work.table <- read.csv("./projector/random/total_work.csv")
-#makespan.table <-read.csv("./projector/random/total_time.csv")
-#clusters.table <-read.csv("./projector/random/cluster.csv")
-#throughput.table <-read.csv("./projector/random/throughput.csv")
+total_work.table <- read.csv(f1)
+makespan.table <-read.csv(f2)
+clusters.table <-read.csv(f3)
+throughput.table <-read.csv(f4)
 
 ############################# Define colors  ##################################
 
@@ -92,25 +88,27 @@ total_work.plot <- total_work.plot + theme(text = element_text(size = 20),
                                            axis.text.y = element_text(size = 20),
                                            legend.text = element_text(size = 16),
                                            legend.title = element_blank(),
-                                           legend.position = c(0.055, 0.725),
+                                           legend.position = c(0.055, 0.77),
                                            panel.grid.minor = element_blank(),
                                            panel.grid.major = element_blank())
 
 total_work.plot <- total_work.plot +
-  labs(y = expression(paste("Work x", 10^{4}))) +
+  labs(y = expression(paste("Trabalho total x", 10^{4}))) +
   scale_fill_manual(values = c(opt_color, ftng_color, fn_color, sn_color, dsn_color, bt_color)) +
-  scale_y_continuous(breaks = seq(0, 60000, 10000), labels = function(x){paste0(x/10000)})
+  scale_y_continuous(labels = function(x){paste0(x/10000)})
 
 plot(total_work.plot)
 
-IMG_height = 2.5
+IMG_height = 3
 IMG_width = 7
 
-ggsave(filename = "./plots/projecToR/total_work.pdf", units = "cm",
+f <- paste("./FlatNet/plots/projector/", dataset, "/total_work.pdf", sep = "")
+
+ggsave(filename = f, units = "cm",
        plot = total_work.plot, device = "pdf",  width = IMG_width, height = IMG_height, scale = 4.0)
 
 
-############################# makespan  ##################################
+############################# makespan 1 ##################################
 
 
 makespan.table["abb"] <- revalue(makespan.table$project, 
@@ -155,11 +153,67 @@ plot(makespan.plot)
 IMG_height = 3
 IMG_width = 3
 
-ggsave(filename = "./plots/projecToR/makespan.pdf", units = "cm",
+f <- paste("./FlatNet/plots/projector/", dataset, "/makespan1.pdf", sep = "")
+
+ggsave(filename = f, units = "cm",
        plot = makespan.plot, device = "pdf",  width = IMG_width, height = IMG_height, scale = 4.0)
 
 
-############################# throughput  ##################################
+############################# makespan 2 ##################################
+
+
+makespan.table["abb"] <- revalue(makespan.table$project, 
+                                 c("flattening" = "ftng",
+                                   "flatnet" = "fn",
+                                   "splaynet" = "sn", 
+                                   "displaynet" = "dsn"))
+
+makespan.table$abb <- factor(makespan.table$abb, levels = c("ftng", "fn", "sn", "dsn"))
+
+makespan.table$size <- as.factor(makespan.table$size)
+
+makespan.table %>% filter(
+  abb %in% c("ftng", "fn")) -> makespan.table
+
+# Init Ggplot Base Plot
+makespan.plot <- ggplot(makespan.table, aes(x = size, y = mean, group = abb, color = abb)) +
+  geom_line(size = 1.5) +
+  geom_point(aes(shape = abb), size=3, fill="white") +
+  geom_errorbar(aes(ymin = mean - ((qnorm(0.975)*std)/sqrt(30)), 
+                    ymax = mean + ((qnorm(0.975)*std)/sqrt(30)) ), 
+                width=.2,
+                position="identity") 
+
+# Modify theme components -------------------------------------------
+makespan.plot <- makespan.plot + theme(text = element_text(size = 20),
+                                       plot.title = element_blank(),
+                                       plot.subtitle = element_blank(),
+                                       plot.caption = element_blank(),
+                                       axis.title.x = element_text(size = 25),
+                                       axis.title.y = element_text(size = 25),
+                                       axis.text.x = element_text(size = 20),
+                                       axis.text.y = element_text(size = 15),
+                                       legend.title = element_blank(),
+                                       legend.position = c(0.22, 0.82))
+
+makespan.plot <- makespan.plot + theme(panel.grid.minor = element_blank(),
+                                       panel.grid.major = element_blank()) +
+  labs(x = "#Nodes", y = expression(paste("#Rounds x ", 10^{4}))) +
+  scale_color_manual(values = c(ftng_color, fn_color, sn_color, dsn_color)) +
+  scale_y_continuous(labels = function(x){paste0(x/10000)})
+
+plot(makespan.plot)
+
+IMG_height = 3
+IMG_width = 3
+
+f <- paste("./FlatNet/plots/projector/", dataset, "/makespan2.pdf", sep = "")
+
+ggsave(filename = f, units = "cm",
+       plot = makespan.plot, device = "pdf",  width = IMG_width, height = IMG_height, scale = 4.0)
+
+
+############################# throughput 1  ##################################
 
 
 throughput.table["abb"] <- revalue(throughput.table$project, 
@@ -170,9 +224,6 @@ throughput.table["abb"] <- revalue(throughput.table$project,
 
 throughput.table$abb <- factor(throughput.table$abb, levels = c("ftng", "fn","sn", "dsn"))
 
-
-#throughput.table %>% filter(
-#  abb %in% c("ftng", "fn")) -> throughput.table
 
 # Init Ggplot Base Plot
 throughput.plot <- ggplot(throughput.table, aes(x = value, fill = abb)) +
@@ -193,7 +244,7 @@ throughput.plot <- throughput.plot + theme(text = element_text(size = 20),
 
 throughput.plot <- throughput.plot + theme(panel.grid.minor = element_blank(),
                                            panel.grid.major = element_blank()) +
-  labs(x = expression(paste("Time (rounds)", 10^3)), y = "Requests completed (per round)") +
+  labs(x = expression(paste("Tempo (rounds)", 10^3)), y = "Requisições por round") +
   scale_fill_manual(values = c(ftng_color, fn_color, sn_color, dsn_color)) +
   scale_x_continuous(labels = function(x){paste0(x/1000)})
 
@@ -202,7 +253,58 @@ plot(throughput.plot)
 IMG_height = 2.5
 IMG_width = 7
 
-ggsave(filename = "./plots/projecToR/throughput2.pdf", units = "cm",
+f <- paste("./FlatNet/plots/projector/", dataset, "/throughput1.pdf", sep = "")
+
+ggsave(filename = f , units = "cm",
+       plot = throughput.plot, device = "pdf",  width = IMG_width, height = IMG_height, scale = 4.0)
+
+
+############################# throughput 2  ##################################
+
+
+throughput.table["abb"] <- revalue(throughput.table$project, 
+                                   c("flattening" = "ftng",
+                                     "flatnet" = "fn",
+                                     "splaynet" = "sn", 
+                                     "displaynet" = "dsn"))
+
+throughput.table$abb <- factor(throughput.table$abb, levels = c("ftng", "fn","sn", "dsn"))
+
+
+throughput.table %>% filter(
+  abb %in% c("ftng", "fn")) -> throughput.table
+
+# Init Ggplot Base Plot
+throughput.plot <- ggplot(throughput.table, aes(x = value, fill = abb)) +
+  geom_density(aes(y = ..count..), alpha = 0.5) +
+  facet_grid(. ~ size)
+
+# Modify theme components -------------------------------------------
+throughput.plot <- throughput.plot + theme(text = element_text(size = 20),
+                                           plot.title = element_blank(),
+                                           plot.subtitle = element_blank(),
+                                           plot.caption = element_blank(),
+                                           axis.title.x = element_text(size = 18),
+                                           axis.title.y = element_text(size = 15),
+                                           axis.text.x = element_text(size = 20),
+                                           axis.text.y = element_text(size = 20),
+                                           legend.title = element_blank(),
+                                           legend.position = c(0.15, 0.75))
+
+throughput.plot <- throughput.plot + theme(panel.grid.minor = element_blank(),
+                                           panel.grid.major = element_blank()) +
+  labs(x = expression(paste("Tempo (rounds)", 10^3)), y = "Requisições por round") +
+  scale_fill_manual(values = c(ftng_color, fn_color, sn_color, dsn_color)) +
+  scale_x_continuous(labels = function(x){paste0(x/1000)})
+
+plot(throughput.plot)
+
+IMG_height = 2.5
+IMG_width = 7
+
+f <- paste("./FlatNet/plots/projector/", dataset, "/throughput2.pdf", sep = "")
+
+ggsave(filename = f , units = "cm",
        plot = throughput.plot, device = "pdf",  width = IMG_width, height = IMG_height, scale = 4.0)
 
 
@@ -250,5 +352,59 @@ plot(clusters.plot)
 IMG_height = 7
 IMG_width = 7
 
-ggsave(filename = "./plots/projecToR/clusters2.pdf", units = "cm",
+f <- paste("./FlatNet/plots/projector/", dataset, "/clusters1.pdf", sep = "")
+
+ggsave(filename = f, units = "cm",
+       plot = clusters.plot, device = "pdf",  width = IMG_width, height = IMG_height, scale = 4.0)
+
+############################# cluster 2 ##################################
+
+
+clusters.table["abb"] <- revalue(clusters.table$project, 
+                                 c("flattening" = "ftng",
+                                   "flatnet" = "fn",
+                                   "splaynet" = "sn", 
+                                   "displaynet" = "dsn"))
+
+clusters.table$abb <- factor(clusters.table$abb, levels = c("ftng", "fn", "sn", "dsn"))
+
+clusters.table %>% filter(
+  abb %in% c("ftng", "fn")) -> clusters.table
+
+# Init Ggplot Base Plot
+clusters.plot <- ggplot(clusters.table, aes(x = value, fill = abb)) +
+  geom_histogram(aes(y = ..count..), position = "dodge", binwidth = 1) +
+  facet_grid( abb ~ size) #+
+# Add mean line
+#geom_vline(aes(xintercept=mean(value)), linetype="dashed") 
+
+# Modify theme components -------------------------------------------
+clusters.plot <- clusters.plot + theme(text = element_text(size = 20),
+                                       plot.title = element_blank(),
+                                       plot.subtitle = element_blank(),
+                                       plot.caption = element_blank(),
+                                       axis.title.x = element_text(size = 20),
+                                       axis.title.y = element_text(size = 20),
+                                       axis.text.x = element_text(size = 15),
+                                       axis.text.y = element_text(size = 15),
+                                       legend.title = element_blank(),
+                                       legend.position = "top",#c(0.15, 0.85),
+                                       panel.grid.minor = element_blank(),
+                                       panel.grid.major = element_blank())
+
+clusters.plot <- clusters.plot + 
+  labs(x = "#Clusters", y = expression(paste("#Rounds x", 10^3))) +
+  scale_fill_manual(values = c(ftng_color, fn_color, sn_color, dsn_color)) +
+  coord_cartesian(xlim = c(0, 25)) +
+  scale_y_sqrt(labels = function(x){paste0(x/1000)})
+#breaks = seq(0, 16000, 1000), 
+
+plot(clusters.plot)
+
+IMG_height = 7
+IMG_width = 7
+
+f <- paste("./FlatNet/plots/projector/", dataset, "/clusters2.pdf", sep = "")
+
+ggsave(filename = f, units = "cm",
        plot = clusters.plot, device = "pdf",  width = IMG_width, height = IMG_height, scale = 4.0)
